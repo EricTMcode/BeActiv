ff113711f5ec3d84e2a5e760228200fc1a8db8fd
Eric <ebagnaro@gmail.com>
2023-10-26 14:13:50 +0200

    Initial Commit

---
+++ BeActiv/BeActivApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeActivApp.swift
+//  BeActiv
+//
+//  Created by Eric on 26/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct BeActivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
